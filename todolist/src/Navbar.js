818c2a51a8bcc893544375e6069963391650100e
jatin sidana <jatinsadana1998@gmail.com>
2023-08-22 19:52:11 +0530

    navbar- completed

--- todolist/src/Navbar.js
+++ todolist/src/Navbar.js
@@ -1,20 +1,26 @@
-export const Navbar = () =>{
-    return(
+import { Link } from "react-router-dom"
+export const Navbar = () => {
+    return (
         <>
-        <nav className="navbar-container">
-            <div className="navbar-logo">
-             <span>To Do List</span>
-            </div>
-            <div className="navbar-list">
-             <ul>
-              <li>
-              <Link to={'/'}>Home</Link>
-              </li>
-               
-                
-             </ul>
-             </div>
-        </nav>
+            <nav className="navbar-container">
+                <div className="navbar-logo">
+                    <Link to={'/'}>
+                        <span>To Do List</span>
+                    </Link>
+                </div>
+                <div className="navbar-list">
+                    <ul className="nav-ul">
+                        <li className="nav-li">
+                            <Link to={'/'}>Home</Link>
+                        </li>
+                        <li className="nav-li">
+                            <Link to={'/login'}>Login</Link>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </nav>
         </>
     )
 }
