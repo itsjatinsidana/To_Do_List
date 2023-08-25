7ec07548fff29778d8a12d85a70fd2257b327d53
jatin sidana <jatinsadana1998@gmail.com>
2023-08-25 20:17:34 +0530

    banner completed

--- todolist/src/Home.js
+++ todolist/src/Home.js
@@ -1,7 +1,22 @@
+import { Navbar } from "./Navbar"
+
 export const Home = () => {
-    return(
+    return (
         <>
-        
+            <Navbar />
+
+            <div class="banner">
+                <div className="banner-content">
+                    <h2>Organize your work <br /> and life, finally.</h2>
+                    <p>Become focused, organized, and <br/>calm with Todoist. </p>
+                    <button>Start for free</button>
+                </div>
+                <div className="banner-img">
+                    <img className="banner-image" src="./assets/images/todolist.jpg" alt="image"/>
+                </div>
+
+            </div>
+
         </>
     )
 }
